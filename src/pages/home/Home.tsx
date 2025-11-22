c1128f8bc9e34a48786b62194018a013dae8b4a1
ArifurSajid <arifursajid3456@gmail.com>
2025-11-22 13:20:56 +0600

    Add FAQ TIENDAONLINE

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import HeroOne from './HeroOne';
+import Seccion from './Seccion';
+import FAQ from './FAQ';
+import TiendaOnline  from './tiendaonline'
+import Quedicen from './Quedicen';
 
 const Home = () => {
     return (
+<>
          <HeroOne/>
+
+
+        <Seccion/>
+        <FAQ/>
+        <TiendaOnline/>
+        <Quedicen/>
+</>
     );
 };
 
