b54818d1aad91b9f2bcb43898a59320f01037578
Tyrin Lannister <tyrionlannister.pxl@gmail.com>
2025-11-27 17:27:54 +0600

    dropdown button icon fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import { Button, Menu, MenuItem, IconButton } from "@mui/material";
 import { useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+
 import {
   WhatsApp,
   Instagram,
@@ -49,8 +51,12 @@ const Navbar = () => {
     <>
       <div className="bg-green-800 pr-3 md:pr-8 flex justify-between md:justify-end gap-2">
         <div className="flex justify-center items-center gap-4 text-white pl-3 md:pl-0">
-          <button className="text-[15px]"><a href="/login">Login</a></button>
-          <button className="text-[15px]"><a href="/register">Register</a></button>
+          <button className="text-[15px]">
+            <a href="/login">Login</a>
+          </button>
+          <button className="text-[15px]">
+            <a href="/register">Register</a>
+          </button>
         </div>
         <div className="flex justify-end items-end gap-2">
           <IconButton sx={{ color: "#fff" }}>
@@ -79,9 +85,21 @@ const Navbar = () => {
 
               {/* Desktop Nav */}
               <nav className="hidden xl:flex items-center space-x-10">
-                <Button
+                {/* <Button
                   onClick={handleServicesClick}
                   endIcon={<span className="text-xs">↓</span>}
+                  className={`normal-case text-base font-semibold ${
+                    isServicesActive()
+                      ? "text-[#046838] underline decoration-[#046838] underline-offset-4"
+                      : "text-green-800 hover:text-[#046838]"
+                  }`}
+                >
+                  {t("servicios")}
+                </Button> */}
+
+                <Button
+                  onClick={handleServicesClick}
+                  endIcon={<ExpandMore />}
                   className={`normal-case text-base font-semibold ${
                     isServicesActive()
                       ? "text-[#046838] underline decoration-[#046838] underline-offset-4"
