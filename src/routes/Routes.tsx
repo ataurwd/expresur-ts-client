9fded698250b29ac0d79531b596d92112961e9ef
Sabba Hossain <sabbahossain@gmail.com>
2025-11-29 12:50:17 +0600

    route

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -31,6 +31,7 @@ import AdminConsolidations from "../dashboard/admin-dashboard/AdminConsolidation
 import AdminSettings from "../dashboard/admin-dashboard/AdminSettings";
 import AdminRates from "../dashboard/admin-dashboard/AdminRates";
 import FAQPage from "../pages/FAQPage/FAQPage";
+import DashbordUser from "../dashboard/user-dashboard/DashbordUser";
 
 
 
@@ -95,6 +96,10 @@ const Routes = createBrowserRouter([
       {
         path: "packages",
         element: <Packages/>
+      },
+      {
+        path:"user-dashboard",
+        element: <DashbordUser/>
       },
       {
         path: "consolidate",
