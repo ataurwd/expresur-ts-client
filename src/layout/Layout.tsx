eb57e978a3e3a21377c58a59ae44219be43a5f7b
ataurwd <ataurrahman24707@gmail.com>
2025-11-30 12:23:00 +0600

    connect global state

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import { Outlet } from 'react-router-dom';
-import Footer from '../components/Footer';
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Outlet, useNavigation } from "react-router-dom";
+import Footer from "../components/Footer";
+import Loading from "../components/Loading";
 
 const Layout = () => {
-    return (
-        <div>
-            <Navbar />
-            <div className='min-h-[calc(100vh-220px)]'>
-                <Outlet />
-            </div>
-            <Footer/>
+  const navigation = useNavigation();
+  const isNavigation = navigation.state === "loading";
+  return (
+    <div>
+      {isNavigation ? (
+        <div className="flex justify-center items-center h-screen bg-gray-50">
+          <Loading />
         </div>
-    );
+      ) : (
+        <>
+          <Navbar />
+          <div className="min-h-[calc(100vh-220px)]">
+            <Outlet />
+          </div>
+          <Footer />
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Layout;
