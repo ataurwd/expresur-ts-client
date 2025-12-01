cec93fd98d590b2c26d6a4c9c0eb097abd5b43c6
ArifurSajid <arifursajid3456@gmail.com>
2025-12-01 11:02:50 +0600

    update Admin Panel

--- src/dashboard/admin-dashboard/AdminAdmins.tsx
+++ src/dashboard/admin-dashboard/AdminAdmins.tsx
@@ -9,6 +9,7 @@ import {
   CartesianGrid,
 } from "recharts";
 import { useCurrentUser } from "../../context/useCurrentUser";
+import { Helmet } from "react-helmet";
 
 /** PRIMARY THEME COLOR */
 const PRIMARY = "#166534"; // as you requested (#166534)
@@ -180,6 +181,7 @@ export default function AdminAdmins() {
 
   return (
     <div className="p-6 bg-[#f7faf7] min-h-screen">
+      <Helmet><title>Admin Panel | EXPRESUR</title></Helmet>
       <div className=" mx-auto">
         {/* Header */}
         <div className="flex items-center justify-between mb-6">
