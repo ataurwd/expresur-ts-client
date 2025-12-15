2447b45a391cc6f9e76b523158d2645cd0f16b54
arifurrahmansajid <iamnobody3456@gmail.com>
2025-12-15 13:06:07 +0600

    update faq

--- src/pages/FAQPage/FaqMAin.tsx
+++ src/pages/FAQPage/FaqMAin.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import FaqHero from './FaqHero';
 import FAQPage from './FAQPage';
 
 const FaqMAin = () => {
     return (
         <div>
-            <FaqHero/>
             <FAQPage/>
         </div>
     );
